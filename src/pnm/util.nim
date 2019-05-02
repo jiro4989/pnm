7b2fc28735fc4e98685898babd33bc76fea4a6f4
jiro4989 <jiroron666@gmail.com>
2019-05-02 19:12:43 +0900

    :bug: 8bit以上のときに誤った計算結果を返していた不具合を修正した

--- src/pnm/util.nim
+++ src/pnm/util.nim
@@ -61,7 +61,7 @@ proc toMatrixString*(s: string, col: int): string =
     lines.add line
   result = lines.mapIt(it.join(" ")).join("\n")
 
-proc toBin*(arr: openArray[uint8], bitSize: int =  8): seq[uint8] =
+proc toBin*(arr: openArray[uint8], col: int =  8): seq[uint8] =
   ## Returns sequences that binary sequence is converted to uint8 every 8 bits.
   runnableExamples:
     doAssert @[1'u8, 1, 1, 1, 0, 0, 0, 0].toBin == @[0b1111_0000'u8]
@@ -74,13 +74,17 @@ proc toBin*(arr: openArray[uint8], bitSize: int =  8): seq[uint8] =
     data = data shl 1
     data += u
     i.inc
-    if i mod bitSize == 0:
-      data = data shl (8 - bitSize)
+    if i mod 8 == 0:
+      result.add data
+      data = 0'u8
+      continue
+    if i mod col == 0:
+      data = data shl (8 - (i mod 8))
       result.add data
       data = 0'u8
       i = 0
   if data != 0:
-    result.add data shl (bitSize - i)
+    result.add data shl (8 - (i mod 8))
 
 proc removeCommentLine*(s: openArray[uint8]): seq[uint8] =
   ## Return sequence that removed comment line.
