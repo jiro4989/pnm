03cee29f345f6ec5de764f2e1087aa94dd9ab88e
jiro4989 <jiroron666@gmail.com>
2021-02-04 02:11:31 +0900

    add test

--- tests/test_pbm.nim
+++ tests/test_pbm.nim
@@ -7,8 +7,10 @@ suite "proc readTextDataPartOfPBM":
     discard
 
 suite "proc toBitSeq":
-  test "P1":
-    discard
+  test "0b0000_0000": check 0'u8.toBitSeq == @[0'u8, 0, 0, 0, 0, 0, 0, 0]
+  test "0b1000_0001": check 0b1000_0001.toBitSeq == @[1'u8, 0, 0, 0, 0, 0, 0, 1]
+  test "0b1010_1010": check 0b1010_1010.toBitSeq == @[1'u8, 0, 1, 0, 1, 0, 1, 0]
+  test "0b1111_1111": check 0b1111_1111.toBitSeq == @[1'u8, 1, 1, 1, 1, 1, 1, 1]
 
 suite "proc readBinaryDataPartOfPBM":
   test "P1":
