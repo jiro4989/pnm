243e0f2d934d9d634112df285838d668cf328948
jiro4989 <jiroron666@gmail.com>
2021-02-04 02:08:21 +0900

    add test

---
+++ tests/test_pbm.nim
@@ -0,0 +1,39 @@
+import unittest
+
+include pnm/pbm
+
+suite "proc readTextDataPartOfPBM":
+  test "P1":
+    discard
+
+suite "proc toBitSeq":
+  test "P1":
+    discard
+
+suite "proc readBinaryDataPartOfPBM":
+  test "P1":
+    discard
+
+suite "proc readPBM":
+  test "P1":
+    discard
+
+suite "proc readPBMFile":
+  test "P1":
+    discard
+
+suite "proc bitSeqToByteSeq":
+  test "P1":
+    discard
+
+suite "proc headTail":
+  test "P1":
+    discard
+
+suite "proc writeBinaryDataPartOfPBM":
+  test "P1":
+    discard
+
+suite "proc writePBMFile":
+  test "P1":
+    discard
