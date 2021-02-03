59b497d3fcb55f40d24094f26f8b7d36ce24b2b6
jiro4989 <jiroron666@gmail.com>
2021-02-04 01:25:12 +0900

    add test

--- tests/test_util.nim
+++ tests/test_util.nim
@@ -10,3 +10,21 @@ suite "proc readHeaderPart":
     let want = strm.readHeaderPart
     let got = Header(descriptor: P1, col: 5, row: 3)
     check want == got
+
+  test "P2":
+    var strm = newStringStream("""P2
+5 3
+255
+""")
+    let want = strm.readHeaderPart
+    let got = Header(descriptor: P2, col: 5, row: 3, max: 255'u8)
+    check want == got
+
+  test "P3":
+    var strm = newStringStream("""P3
+5 3
+255
+""")
+    let want = strm.readHeaderPart
+    let got = Header(descriptor: P3, col: 5, row: 3, max: 255'u8)
+    check want == got
