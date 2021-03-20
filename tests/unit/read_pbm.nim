611d8a196035d0f6f7dfb11a864f159f848d26a7
jiro4989 <jiroron666@gmail.com>
2021-03-20 18:49:43 +0900

    add test case

---
+++ tests/unit/read_pbm.nim
@@ -0,0 +1,64 @@
+import pnm
+
+import streams, unittest
+
+# normal case
+block:
+  const inData = """
+P1
+5 3
+0 1 0 1 0
+1 0 1 0 1
+0 1 0 1 0
+"""
+  const inData2 = """
+P1
+5 3
+0 1 0 1 0 1 0 1 0 1 0 1 0 1 0"""
+  const inIllegalData = """
+P1
+5 3
+0 1 0 1 0
+1 0 1 0 1
+0 1 0 1 0
+1 1 1 1 1
+""" # too many data
+
+  var want = newPbm(P1, 5, 3)
+  const row1 = [0'u8, 1, 0, 1, 0]
+  const row2 = [1'u8, 0, 1, 0, 1]
+  proc setRow(y: int, row: array[5, byte]) =
+    for x in 0..<5:
+      want[x, y] = row[x]
+  setRow(0, row1)
+  setRow(1, row2)
+  setRow(2, row1)
+
+  # ============
+  # normal cases
+  # ============
+
+  block:
+    # multi line case
+    var strm = newStringStream(inData)
+    var got = strm.readPbm
+    check got[] == want[]
+    strm.close
+
+  block:
+    # 1 line case
+    var strm = newStringStream(inData2)
+    var got = strm.readPbm
+    check got[] == want[]
+    strm.close
+
+  # ==============
+  # illegal casses
+  # ==============
+
+  block:
+    var strm = newStringStream(inIllegalData)
+    var got = strm.readPbm
+    check got[] == want[]
+    strm.close
+
